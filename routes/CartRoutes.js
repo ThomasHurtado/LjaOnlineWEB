7b33de1c968b9c6c36c4721c4d240032ce0e47a0
ThomasHurtado <thomashurtado@outlook.com.br>
2024-06-25 12:19:36 -0300

    erros

--- routes/CartRoutes.js
+++ routes/CartRoutes.js
@@ -9,6 +9,6 @@ router.patch('/removeone/:id', checkToken, CartController.removeUnitFromCart)
 router.get('/getcart', checkToken, CartController.allCartProducts)
 router.patch('/deleteproduct/:id', checkToken, CartController.removeItemFromCart)
 router.get('/usercart', checkToken, CartController.userCart)
-router.patch('clearcart', checkToken, CartController.clearCart)
+router.patch('/clearcart', checkToken, CartController.clearCart)
 
 module.exports = router
